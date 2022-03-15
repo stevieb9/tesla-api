cc8565dffb789eada2dd5faa93f856b8dca22c1c
Steve Bertrand <steveb@cpan.org>
2022-03-15 11:21:12 -0700

    Initial import of Tesla::API

---
+++ lib/Tesla/API.pm
@@ -0,0 +1,52 @@
+package Tesla::API;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+sub __placeholder {}
+
+1;
+__END__
+
+=head1 NAME
+
+Tesla::API - One line description
+
+=for html
+<a href="https://github.com/stevieb9/tesla-api/actions"><img src="https://github.com/stevieb9/tesla-api/workflows/CI/badge.svg"/></a>
+<a href='https://coveralls.io/github/stevieb9/tesla-api?branch=master'><img src='https://coveralls.io/repos/stevieb9/tesla-api/badge.svg?branch=master&service=github' alt='Coverage Status' /></a>
+
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head2 name
+
+Description.
+
+I<Parameters>:
+
+    $bar
+
+I<Mandatory, String>: The name of the thing with the guy and the place.
+
+I<Returns>: C<0> upon success.
+
+=head1 AUTHOR
+
+Steve Bertrand, C<< <steveb at cpan.org> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2022 Steve Bertrand.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at:
+
+L<http://www.perlfoundation.org/artistic_license_2_0>
